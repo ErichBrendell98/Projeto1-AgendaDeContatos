29fe41dafc45c7c328914f6ed673094838b9a718
Erich Brendell <erychbrendell@gmail.com>
2022-08-30 10:07:49 -0300

    modified:agenda.py

--- agenda.py
+++ agenda.py
@@ -12,12 +12,37 @@ AGENDA['maria'] = {
     'endereco':'Av. 2',
 }
 
+def buscar_contato(contato):
+    print(f'Nome: {contato}')
+    print(f'Telefone: {AGENDA[contato]["telefone"]}')
+    print(f'Email: {AGENDA[contato]["email"]}')
+    print(f'Endereco: {AGENDA[contato]["endereco"]}')
+
+
 def mostrar_contatos():
     for contato in AGENDA:
-        print(f"Nome: {contato}")
-        print(f"Telefone: {AGENDA[contato]['telefone']}")
-        print(f"Email: {AGENDA[contato]['email']}")
-        print(f"Endereco: {AGENDA[contato]['endereco']}")
+        buscar_contato(contato)
         print(f'{"-"*80}')
 
+
+def incluir_editar_contato(contato, telefone, email, endereco):
+    AGENDA[contato] = {
+        'telefone':telefone,
+        'email':email,
+        'endereco':endereco, 
+    }
+    print(f'>>>>>> {contato} adicionado/editado com sucesso.')
+
+
+def excluir_contato(contato):
+    AGENDA.pop(contato)
+    print(f'>>>>>> {contato} excluido com sucesso.')
+
+
+incluir_editar_contato('joao', '987654321', 'joao@solyd.com.br', 'Av. 3')
+incluir_editar_contato('erich', '555555555', 'contato@solyd.com.br', 'Av.1')
+incluir_editar_contato('jose', '666666666', '', None)
+excluir_contato('maria')
 mostrar_contatos()
+
+# buscar_contato('erich')
